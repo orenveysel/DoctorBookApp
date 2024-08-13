0169b894e0c9f75b620a298a710941725251943b
Veysel ÖREN <orenveysel@outlook.com>
2024-08-13 23:17:41 +0300

    modified login and register page

--- DoctorBookApp/Models/LoginViewModel.cs
+++ DoctorBookApp/Models/LoginViewModel.cs
@@ -4,8 +4,11 @@ namespace DoctorBookApp.WebMvc.Models
 {
     public class LoginViewModel
     {
+        [Display(Name = "National ID")]
         [Range(10000000000, 99999999999, ErrorMessage = "National ID must be 11 digits.")]
         public long NationalId { get; set; }
+
+        [Display(Name = "Password")]
         public string Password { get; set; }
     }
 }
--- DoctorBookApp/Models/RegisterViewModel.cs
+++ DoctorBookApp/Models/RegisterViewModel.cs
@@ -4,22 +4,35 @@ namespace DoctorBookApp.WebMvc.Models
 {
     public class RegisterViewModel
     {
+        [Display(Name = "National ID")]
         [Range(10000000000, 99999999999, ErrorMessage = "National ID must be 11 digits.")]
         public long NationalId { get; set; }
+
+        [Display(Name = "Birth Date")]
         public DateTime BirthDate { get; set; }
+
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+
+        [Display(Name = "Gender")]
         public string Gender { get; set; }
 
+        [Display(Name = "Phone Number")]
         [Range(1000000000, 9999999999, ErrorMessage = "Phone number must be 10 digits.")]
         public long PhoneNumber { get; set; }
 
+        [Display(Name = "Email")]
         [EmailAddress(ErrorMessage = "Invalid email address.")]
         public string Email { get; set; }
 
+        [Display(Name = "Password")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
+        [Display(Name = "Confirm Password")]
         [DataType(DataType.Password)]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
