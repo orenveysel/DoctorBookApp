dc5c168e84f2bf6f052960655fe2b73c8292f144
Veysel ÖREN <orenveysel@outlook.com>
2024-08-13 20:31:55 +0300

    added comparing password and confirm password in register page

--- DoctorBookApp/Models/RegisterViewModel.cs
+++ DoctorBookApp/Models/RegisterViewModel.cs
@@ -1,4 +1,6 @@
-﻿namespace DoctorBookApp.WebMvc.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DoctorBookApp.WebMvc.Models
 {
     public class RegisterViewModel
     {
@@ -9,7 +11,12 @@
         public string Gender { get; set; }
         public int PhoneNumber { get; set; }
         public string Email { get; set; }
+
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
 
     }
